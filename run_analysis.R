8455528ed1ed16e37da1e05de647dc30c760b281
unknown <khamgaonkarg@DPCT210387.premiers.qld.gov.au>
2015-02-13 15:41:48 +1000

    added run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,36 @@
+mergeFile <-function (testFN,trainFN, mergedFN)
+{
+        if(file.exists(mergedFN))
+        {
+                unlink(mergedFN)
+        }
+        file.append(mergedFN , testFN)
+        file.append(mergedFN , trainFN)
+}
+
+mergeFiles <-function ()
+{
+        # create the merged folder
+        if(!file.exists("data/merged"))
+        {
+                dir.create("data/merged")        
+        }
+        
+        if(!file.exists("data/merged/Inertial Signals"))
+        {
+                dir.create("data/merged/Inertial Signals")        
+        }
+        
+        mergeFile("y_test.txt","data/train/y_train.txt" , "data/merged/y_merged.txt" )
+        mergeFile("data/test/X_test.txt","data/train/X_train.txt" , "data/merged/X_merged.txt" )
+        mergeFile("data/test/subject_test.txt","data/train/subject_train.txt" , "data/merged/subject_merged.txt" )
+        mergeFile("data/test/Inertial Signals/body_acc_x_test.txt","data/train/Inertial Signals/body_acc_x_train.txt","data/merged/Inertial Signals/body_acc_x_merged.txt")
+        mergeFile("data/test/Inertial Signals/body_acc_y_test.txt","data/train/Inertial Signals/body_acc_y_train.txt","data/merged/Inertial Signals/body_acc_y_merged.txt")
+        mergeFile("data/test/Inertial Signals/body_acc_z_test.txt","data/train/Inertial Signals/body_acc_z_train.txt","data/merged/Inertial Signals/body_acc_z_merged.txt")
+        mergeFile("data/test/Inertial Signals/body_gyro_x_test.txt","data/train/Inertial Signals/body_gyro_x_train.txt","data/merged/Inertial Signals/body_gyro_x_merged.txt")
+        mergeFile("data/test/Inertial Signals/body_gyro_y_test.txt","data/train/Inertial Signals/body_gyro_y_train.txt","data/merged/Inertial Signals/body_gyro_y_merged.txt")
+        mergeFile("data/test/Inertial Signals/body_gyro_z_test.txt","data/train/Inertial Signals/body_gyro_z_train.txt","data/merged/Inertial Signals/body_gyro_z_merged.txt")
+        mergeFile("data/test/Inertial Signals/body_acc_x_test.txt","data/train/Inertial Signals/body_acc_x_train.txt","data/merged/Inertial Signals/body_acc_x_merged.txt")
+        mergeFile("data/test/Inertial Signals/body_acc_y_test.txt","data/train/Inertial Signals/body_acc_y_train.txt","data/merged/Inertial Signals/body_acc_y_merged.txt")
+        mergeFile("data/test/Inertial Signals/body_acc_z_test.txt","data/train/Inertial Signals/body_acc_z_train.txt","data/merged/Inertial Signals/body_acc_z_merged.txt")
+}
