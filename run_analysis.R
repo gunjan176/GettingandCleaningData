0882dbffcb9556eb769b954df5a714ed4f0374e9
gunjan176 <gunjan176@yahoo.com>
2015-02-21 17:16:39 +1000

    added functions and details

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,8 @@
 ##
 # MergeFile merges content of two files passed as parameter and returns a single data table containing consolidated data
 # Assumption :: the data in  testdata and training data has similar structure  
+# Requirment Addressed :: 
+#       1. Merges the training and the test sets to create one data set.
 # Parameters :: 
 #         1. testFN - File Name of the test data  
 #         2. trainFN - File Name of the training data  
@@ -22,7 +24,9 @@ mergeFile <-function (testFN,trainFN)
 ##
 # mergeAllFiles merges all the test and train data required as a part of the course project
 # and returns a data table which contains all the data which is required for the analysis.
-# Requirment Addressed :: 1. Merges the training and the test sets to create one data set.
+# Requirment Addressed :: 
+#       1. Merges the training and the test sets to create one data set.
+#       3. Uses descriptive activity names to name the activities in the data set
 # Parameters :: None
 # Returns ::
 #        Data table containing the merged. 
@@ -54,7 +58,9 @@ mergeAllFiles <-function ()
 
 ##
 # extractMeanAndSD extracts mean and standard deviation fields from the merged data table and renames the fields names to a more human decipherable format. 
-# Requirment Addressed :: 2. Extracts only the measurements on the mean and standard deviation for each measurement." 
+# Requirment Addressed :: 
+#        2. Extracts only the measurements on the mean and standard deviation for each measurement." 
+#        4. Appropriately labels the data set with descriptive variable names
 # Parameters :: 
 #       1. mergedDataTable : data table containing the merged data.
 # Returns ::
@@ -63,12 +69,41 @@ mergeAllFiles <-function ()
 extractMeanAndSD <-function(mergedDataTable)
 {
         # select all the relavant data - (fields containing mean and standard deviations)
-        mergedDataTable<-select(mergedDataTable,C("ActivityName","Label","Subject","V1","V2","V3","V4","V5","V6","V41","V42","V43","V44","V45","V46","V81","V82","V83","V84","V85","V86","V121","V122","V123","V124","V125","V126","V161","V162","V163","V164","V165","V166","V266","V267","V268","V269","V270","V271","V345","V346","V347","V348","V349","V350","V424","V425","V426","V427","V428","V429"))
+        mergedDataTable<-select(mergedDataTable,ActivityName,Label,Subject,V1,V2,V3,V4,V5,V6,V41,V42,V43,V44,V45,V46,V81,V82,V83,V84,V85,V86,V121,V122,V123,V124,V125,V126,V161,V162,V163,V164,V165,V166,V266,V267,V268,V269,V270,V271,V345,V346,V347,V348,V349,V350,V424,V425,V426,V427,V428,V429)
         
-        setnames(mergeAllFiles
+        setnames(mergedDataTable
                  ,c("V1","V2","V3","V4","V5","V6","V41","V42","V43","V44","V45","V46","V81","V82","V83","V84","V85","V86","V121","V122","V123","V124","V125","V126","V161","V162","V163","V164","V165","V166","V266","V267","V268","V269","V270","V271","V345","V346","V347","V348","V349","V350","V424","V425","V426","V427","V428","V429")
                  ,c("tBodyAcc_mean_X","tBodyAcc_mean_Y","tBodyAcc_mean_Z","tBodyAcc_std_X","tBodyAcc_std_Y","tBodyAcc_std_Z","tGravityAcc_mean_X","tGravityAcc_mean_Y","tGravityAcc_mean_Z","tGravityAcc_std_X","tGravityAcc_std_Y","tGravityAcc_std_Z","tBodyAccJerk_mean_X","tBodyAccJerk_mean_Y","tBodyAccJerk_mean_Z","tBodyAccJerk_std_X","tBodyAccJerk_std_Y","tBodyAccJerk_std_Z","tBodyGyro_mean_X","tBodyGyro_mean_Y","tBodyGyro_mean_Z","tBodyGyro_std_X","tBodyGyro_std_Y","tBodyGyro_std_Z","tBodyGyroJerk_mean_X","tBodyGyroJerk_mean_Y","tBodyGyroJerk_mean_Z","tBodyGyroJerk_std_X","tBodyGyroJerk_std_Y","tBodyGyroJerk_std_Z","fBodyAcc_mean_X","fBodyAcc_mean_Y","fBodyAcc_mean_Z","fBodyAcc_std_X","fBodyAcc_std_Y","fBodyAcc_std_Z","fBodyAccJerk_mean_X","fBodyAccJerk_mean_Y","fBodyAccJerk_mean_Z","fBodyAccJerk_std_X","fBodyAccJerk_std_Y","fBodyAccJerk_std_Z","fBodyGyro_mean_X","fBodyGyro_mean_Y","fBodyGyro_mean_Z","fBodyGyro_std_X","fBodyGyro_std_Y","fBodyGyro_std_Z"))
-        mergeAllFiles
+        mergedDataTable
 
 }
+##
+# summarizeData groups the ActivityName, Label and Subject Details and calculates the mean for each of the fields  
+# Requirment Addressed :: 
+#        5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+# Parameters :: 
+#       1. mergedDataTable : data table containing the merged data containing only the fields which need to be analysed. .
+# Returns ::
+#        An independent tidy data set with the average of each variable for each activity and each subject.
+##
+summarizeData<-function(mergedDataTable)
+{
+        group_by(mergedDataTable,ActivityName, Label, Subject)%>% summarise_each(funs(mean))
+}
+
+
+##
+# mainFunction is the entrypoint to the analysis , it calls various functions mentioned above and exports the results into out.txt file
+# Parameters :: 
+#        none 
+# Returns ::
+#       nothing 
+##
+mainFunction<-function()
+{
+        summaryData<- summarizeData(extractMeanAndSD(mergeAllFiles()))
+        write.table(summaryData,file="data/output.txt",row.name=F)
+}
+
+
         
